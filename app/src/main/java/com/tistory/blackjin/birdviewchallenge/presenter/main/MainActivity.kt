da31d1095e409c021d1c159a5795f3de3ef04157
iwedding <dlwls5201@naver.com>
2020-01-09 12:10:29 +0900

    find list bottom

--- app/src/main/java/com/tistory/blackjin/birdviewchallenge/presenter/main/MainActivity.kt
+++ app/src/main/java/com/tistory/blackjin/birdviewchallenge/presenter/main/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.tistory.blackjin.birdviewchallenge.presenter.main
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
+import androidx.recyclerview.widget.RecyclerView
 import com.tistory.blackjin.birdviewchallenge.R
-import com.tistory.blackjin.birdviewchallenge.presenter.adapter.ProductAdapter
-import com.tistory.blackjin.birdviewchallenge.presenter.adapter.itemdcoration.ProductItemDecoration
 import com.tistory.blackjin.birdviewchallenge.data.ProductRepository
 import com.tistory.blackjin.birdviewchallenge.data.error.MyHttpException
+import com.tistory.blackjin.birdviewchallenge.presenter.adapter.ProductAdapter
+import com.tistory.blackjin.birdviewchallenge.presenter.adapter.itemdcoration.ProductItemDecoration
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,13 +47,28 @@ class MainActivity : AppCompatActivity() {
             adapter = productAdapter
 
             addItemDecoration(ProductItemDecoration())
+
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val lm = recyclerView.layoutManager
+                    if (lm is GridLayoutManager) {
+                        if (lm.findLastCompletelyVisibleItemPosition() ==
+                            recyclerView.adapter?.itemCount?.minus(1)
+                        ) {
+                            Toast.makeText(recyclerView.context, "Bottom", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
         }
 
         productRepo.get()
             .subscribe({
                 productAdapter.replaceAll(it)
             }) {
-                if(it is MyHttpException) {
+                if (it is MyHttpException) {
                     Timber.d("code : ${it.code} , message : ${it.message}")
                 } else {
                     Timber.e(it)
